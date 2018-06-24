bb5b75a013580b2ff0998fd4133a11272f5a6101
Elliot Yandzio <elliotyandzio@gmail.com>
2018-06-24 18:06:18 +0100

    added the new version of the portfolio

--- js/app.js
+++ js/app.js
@@ -1,3 +1,54 @@
-function show() {
-  document.getElementById('project1').style.visibility = 'visible';
-}
+$(document).ready(function() {
+
+  var $win = $(window);
+  var winH = $win.height();   // Get the window height.
+  var winH1 = (winH - 52);
+  var winH2 = ((winH * 2) - 52);
+  var winH3 = ((winH * 3) - 52);
+
+    $win.on("scroll", function () {
+      if ($(this).scrollTop() < winH1) {
+        $( "nav" ).removeClass( "is-danger" );
+        $( "nav" ).removeClass( "is-black" );
+        $( "nav" ).addClass( "is-white" );
+      }
+      if ($(this).scrollTop() > winH1) {
+        $( "nav" ).removeClass( "is-white" );
+        $( "nav" ).addClass( "is-danger" );
+      }
+      if ($(this).scrollTop() > winH2) {
+        $( "nav" ).removeClass( "is-danger" );
+        $( "nav" ).addClass( "is-black" );
+      }
+      if($(this).scrollTop() > winH3) {
+        $( "nav" ).removeClass( "is-black" );
+        $( "nav" ).addClass( "is-white" );
+      }
+    }).on("resize", function(){ // If the user resizes the window
+       winH = $(this).height(); // you'll need the new height value
+    });
+
+    $("#one").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#project1").offset().top
+        }, 2000);
+    });
+
+    $("#two").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#project2").offset().top
+        }, 2000);
+    });
+
+    $("#three").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#project3").offset().top
+        }, 2000);
+    });
+
+    $("#four").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#project4").offset().top
+        }, 2000);
+    });
+});
